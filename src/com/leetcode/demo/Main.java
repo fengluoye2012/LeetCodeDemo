02c154ad6c10591a0b08431544f0649baae8f31e
fengluoye2012 <1522988552@qq.com>
2021-03-28 19:47:14 +0800

    Java-基础

--- src/com/leetcode/demo/Main.java
+++ src/com/leetcode/demo/Main.java
@@ -1,17 +1,7 @@
 package com.leetcode.demo;
 
-import com.leetcode.demo.base.BinarySearch;
-import com.leetcode.demo.base.InsertSort;
-import com.leetcode.demo.base.QuickSort;
-import com.leetcode.demo.base.ReverseSingleLinkedList;
-import com.leetcode.demo.middle.array.LengthOfLongestSubstring;
-import com.leetcode.demo.middle.array.LongestPalindromeStr;
-import com.leetcode.demo.middle.array.ThreeSum2;
-import com.leetcode.demo.simple.array.RemoveDuplicates;
-import com.leetcode.demo.simple.dynamicPlanning.MaxProfit2;
-import com.leetcode.demo.simple.dynamicPlanning.MaxSubArray;
-import com.leetcode.demo.simple.linkedList.HasCycle;
-import com.leetcode.demo.thread.ThreadSynchronizationDemo;
+import com.leetcode.demo.java.base.CodeBlock;
+import com.leetcode.demo.java.base.Swallow;
 
 public class Main {
 
@@ -60,7 +50,12 @@ public class Main {
 //        ThreeSum2.test();
 //        LengthOfLongestSubstring.test();
 
-        LongestPalindromeStr.test();
+//        LongestPalindromeStr.test();
+
+//        CodeBlock test = new CodeBlock();
+//        test.println();
+
+        Swallow swallow = new Swallow();
     }
 
 }
